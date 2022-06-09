bda46e7cb5047e8de7066ccc66562e30a6687b15
unknown <gruppasmerty@gmail.com>
2022-06-09 07:20:34 +0700

    Центр сетки совпадает с центром floor

--- Assets/scripts/GridGenerator.cs
+++ Assets/scripts/GridGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
@@ -8,20 +9,25 @@ public class GridGenerator : MonoBehaviour
     [SerializeField] private Cell _prefab;
     [SerializeField] private float _offset;
     [SerializeField] private Transform _parent;
+    private Vector3 cellSize;
 
     void Start()
     {
-        GenerateGrid();
+        cellSize = _prefab.GetComponent<MeshRenderer>().bounds.size;
+        GameObject floor = GameObject.FindGameObjectWithTag("Floor");
+        var floorSize = floor.GetComponent<MeshRenderer>().bounds.size;
+        GenerateGrid(floorSize.x, floorSize.z);
     }
 
     [ContextMenu ("Generate Grid")]
-    private void GenerateGrid()
+    private void GenerateGrid(float floorX, float floorZ)
     {
-        var cellSize = _prefab.GetComponent<MeshRenderer>().bounds.size;
-        for(int x = 0; x < _gridSize.x; x++)
+        var halfX = floorX/2.2;
+        var halfZ = floorZ/2.2;
+        for(int x = -(int)halfX; x < _gridSize.x -(int)halfX; x++)
         {
-            for (int y = 0; y<_gridSize.y; y++) 
-            {
+            for (int y = -(int)halfZ; y < _gridSize.y - (int)halfZ; y++) 
+            { 
                 var position = new Vector3(x*(cellSize.x+_offset),0,y* (cellSize.z + _offset));
                 var cell = Instantiate(_prefab,position,Quaternion.identity, _parent);
                 cell.name = $"X = {x} Y = {y}";
